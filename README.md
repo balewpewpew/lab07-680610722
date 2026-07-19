4bdfdd09c7bfa8735d1f4240d358c2fa6a82df40
D. Potikanond <potikanond@gmail.com>
2026-07-19 18:41:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Lab 07 - Student API (API #1)
+
+### ป้อนข้อมูลนักศึกษา
+
+รหัส นศ.:
+
+ชื่อ-สกุล :
+
+Vercel URL : 
+
+---
+
+หลังจากการ Fork และ Clone repository แล้ว, ให้เปิดโฟลเดอร์ด้วย VSCode และรันคำสั่งใน terminal:
+
+```bash
+pnpm install
+pnpm run dev
+```
+
+ให้นักศึกษาสร้างระบบ API สำหรับจัดการข้อมูลนักศึกษาอย่างง่าย โดยใช้ความรู้เรื่องการพัฒนา RESTFul API ด้วย Express + TypeScript
+
+กรอก Github Repo URL และ Vercel URL ของ นศ. ลงใน Spreadsheet ต่อไปนี้
+
+[คลิกเพื่อเปิด Spreadsheet](https://docs.google.com/spreadsheets/d/1spJejV7izRS3AtCi4UTHscLe1C3cos6TrOBVJ4hdrmU/edit?usp=sharing)
+
+---
---
+++ src/index.ts
@@ -0,0 +1,164 @@
+import express, { type Request, type Response } from 'express';
+
+// import middleware
+import morgan from "morgan";
+
+// import database
+import { students } from '@db/db.js';
+import { type Student, type Course } from "@libs/types.js";
+import {
+  zStudentDeleteBody,
+  zStudentPostBody,
+  zStudentPutBody,
+} from "@libs/studentValidator.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// use middleware
+app.use(morgan("dev", { immediate: false }));
+app.use(express.json());    // parses request's payload into 'req.body'
+
+// Endpoints
+app.get("/", (req: Request, res: Response) => {
+  res.send("API services for Student Data");
+});
+
+// GET /students
+// get students (by program)
+app.get("/students", (req: Request, res: Response) => {
+  try {
+    const program = req.query.program;
+
+    if (program) {
+      let filtered_students = students.filter(
+        (student) => student.program === program
+      );
+      return res.json({
+        success: true,
+        data: filtered_students,
+      });
+    } else {
+      return res.json({
+        success: true,
+        count: students.length,
+        data: students,
+      });
+    }
+  } catch (err) {
+    return res.json({
+      success: false,
+      message: "Something is wrong, please try again",
+      error: err,
+    });
+  }
+});
+
+// POST /students, body = {new student data}
+// add a new student
+app.post("/students", (req: Request, res: Response) => {
+  try {
+    const body = req.body as Student;
+
+    // validate req.body with predefined validator
+    const result = zStudentPostBody.safeParse(body); // check zod
+    if (!result.success) {
+      return res.json({
+        message: "Validation failed",
+        errors: result.error.issues[0]?.message,
+      });
+    }
+
+    //check duplicate studentId
+    const found = students.find(
+      (student) => student.studentId === body.studentId
+    );
+    if (found) {
+      return res.json({
+        success: false,
+        message: "Student is already exists",
+      });
+    }
+
+    // add new student
+    const new_student = body;
+    students.push(new_student);
+
+    // add response header 'Link'
+    res.set("Link", `/students/${new_student.studentId}`);
+
+    return res.json({
+      success: true,
+      data: new_student,
+    });
+    // return res.json({ ok: true, message: "successfully" });
+  } catch (err) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: err,
+    });
+  }
+});
+
+// PUT /students, body = {studentId}
+// Update specified student
+app.put("/students", (req: Request, res: Response) => {
+  try {
+    const body = req.body as Student;
+
+    // validate req.body with predefined validator
+    const result = zStudentPutBody.safeParse(body); // check zod
+    if (!result.success) {
+      return res.json({
+        message: "Validation failed",
+        errors: result.error.issues[0]?.message,
+      });
+    }
+
+    //check duplicate studentId
+    const foundIndex = students.findIndex(
+      (student) => student.studentId === body.studentId
+    );
+
+    if (foundIndex === -1) {
+      return res.json({
+        success: false,
+        message: "Student does not exists",
+      });
+    }
+
+    // update student data
+    students[foundIndex] = { ...students[foundIndex], ...body };
+
+    // add response header 'Link'
+    res.set("Link", `/students/${body.studentId}`);
+
+    return res.json({
+      success: true,
+      message: `Student ${body.studentId} has been updated successfully`,
+      data: students[foundIndex],
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      message: "Somthing is wrong, please try again",
+      error: err,
+    });
+  }
+});
+
+// DELETE /students, body = {studentId}
+app.delete("/students", (req: Request, res: Response) => {
+  res.json({
+    message: "Implement this!"
+  })
+});
+
+// GET /api/me
+
+app.listen(port, async () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
+});
+
+export default app;
